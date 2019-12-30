a60428d603d5b440f5b61a8e18eb049afa401cf4
AssanSav <assane.savadogo81@gmail.com>
2019-12-30 11:23:59 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,10 +1,10 @@
 module SongsHelper
 
     def artist_select(song)
-        if song.artist.nil?
+        if song.artist.nil? # should list artists drop-down when editing via songs/:id/edit
             select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-        else
-            hidden_field_tag "song[artist_id]", song.artist_id
+        else #should show only an artist name when editing via a nested route
+            hidden_field_tag "song[artist_id]", song.artist_name
         end
     end
 end
